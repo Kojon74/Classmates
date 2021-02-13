c52c5407a1fe7d1901a77a0857c8de223d85d44f
menghanzhao <menghan1@ualberta.ca>
2021-02-13 16:31:33 -0700

    Home Page update (adding title bar and sign in / sign up)

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+     Navbar
+    </div>
+  );
 };
 
 export default Navbar;
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import './home.css'
+import classmates from '../../assets/classmates.jpg'
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Jumbotron style={{ backgroundImage: `url(${classmates})`, }}>
+        <h1>Classmates</h1>
+        <div style={{ width: "450px" }}>
+          <p className="titlePara">
+          Meeting your classmates online, and start collaborating now!
+          </p>
+        </div>
+        <p>
+          <Button variant="primary">Sign up</Button>
+          <Button variant="light" className="signInButton">Sign in</Button>
+        </p>
+      </Jumbotron>
+    </div>
+  );
 };
+
 
 export default Home;
