f54b6aff215c4d112a1bbea5cb16c3886520c6b0
menghanzhao <menghan1@ualberta.ca>
2021-02-14 02:55:59 -0700

    adding logo

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
+import logo from './assets/logo.png';
 
 const Navbar = () => {
   return (
     <nav className="nav">
+      <img src={logo} alt="logo" width="55px"/>
       <h1 className="nav-header">Classmates</h1>
       <Button
         onClick={(event) => (window.location.href = "/")}
